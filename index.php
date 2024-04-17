40ab00f8d349d6a8b0d475cc3a6430860aa921e9
sarkanss <alberts.sarkans@gmail.com>
2024-04-17 14:38:41 +0300

    uzlaboju kodu

--- index.php
+++ index.php
@@ -340,42 +340,119 @@ while ($row = mysqli_fetch_assoc * $result) {
       left: 900px;
       position: absolute;
     }
+	.username
+{
+ width: 300px;
+  height: 60px;
+  font-size: 30px;
+  top: 200px; 
+   left: 700px; 
+   position: absolute; 
+}
+.username_input
+{
+width: 300px;
+  height: 40px;
+  font-size: 30px;
+  top: 270px; 
+  left: 700px; 
+   position: absolute; 
+}
+.password
+{
+ width: 300px;
+  height: 60px;
+  font-size: 30px;
+  top: 300px; 
+   left: 700px; 
+   position: absolute; 
+}
+.password_input
+{
+width: 300px;
+  height: 40px;
+  font-size: 30px;
+  top: 370px; 
+  left: 700px; 
+   position: absolute; 
+}
+.Login_button
+{
+background-color: white;
+  border: none;
+  color: black;
+  width: 150px;
+  height: 30px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 14px;
+  margin: 4px 2px;
+  cursor: pointer;
+  position: absolute; 
+  top: 450px; 
+  left: 850px; 
+}
   </style>
 </head>
 
 <body style="background-color:powderblue;">
 
-  <h1 onclick="Refresh()" style="font-size: 50px">Airlines</h1>
-  <button onclick="Arrival()" class="Arrival" id="Arrival">Arrival </button>
-  <button onclick="Departure()" id="Departure" class="Departure">Departure </button>
-  <button onclick="Ticketpurchase()" class="Ticketpurchase">Ticket purchase </button>
-  <hr style="height: 62px; background-color: white;">
-  <button class="Aboutus">About us </button>
-  <button class="Login">Login </button>
-  <button class="Register">Register</button>
-  <button class="Profile" onclick="Profile()">Profile </button>
-  <button class="Tickets">Tickets </button>
-  <button class="Logout">Log Out </button>
-
-  <h1 id="SearchFlight" style="display: none;">Search Flight:</h1>
-  <input id="FlightSearch" class="FlightSearch" style=" display: none;" type="text" placeholder="Flight number, Destination, Airport name/code">
-  <p class="Time" style="display: none;" id="Time">Time: </p>
-  <input type="datetime-local" id="TimeInput" style=" display: none;" class="TimeInput">
-  <p class="Arrivedfrom" style="display: none;" id="Arrived_from">Arrived from: </p>
-  <input type="text" id="Arrived_from_input" style=" display: none;" class="Arrived_from_input">
-  <p class="Terminal" style="display: none;" id="Terminal">Terminal: </p>
-  <input type="text" id="Terminal_input" style=" display: none;" class="Terminal_input">
-  <p class="Status" style="display: none;" id="Status">Status: </p>
-  <input type="text" id="Status_input" style=" display: none;" class="Status_input">
-  <p class="Start_point" style="display: none;" id="Start_point">Starting point: </p>
-  <input type="text" id="Start_point_input" style=" display: none;" class="Start_point_input">
-  <p class="Destination" style="display: none;" id="Destination">Destination: </p>
-  <input type="text" id="Destination_input" style=" display: none;" class="Destination_input">
-  <input type="datetime-local" id="Travel_date" style=" display: none;" class="Travel_date">
-  <input type="radio" id="First_class" style=" display: none;" class="First_class" name="fav_language">
-  <p class="First_class_label" style=" display: none;" id="First_class_label"> First class</p>
-  <input type="radio" id="Economy" style=" display: none;" class="Economy" name="fav_language">
-  <p class="Economy_label" style=" display: none;" id="Economy_label"> Economy</p>
+  <body style="background-color:powderblue;">
+     <! –– main screen pogas-->
+    <h1 onclick="Refresh()" style="font-size: 50px">Airlines</h1>
+    <button onclick="Arrival()" class="Arrival" id="Arrival">Arrival </button>
+	<button onclick="Departure()" id="Departure"class="Departure">Departure </button>
+	<button onclick="Ticketpurchase()" class="Ticketpurchase">Ticket purchase </button>
+	<hr style="height: 62px; background-color: white;">
+	<button class="Aboutus">About us </button>
+	<button class="Login" onclick="Login()">Login </button>
+	<button class="Register">Register</button>
+	<button class="Profile" onclick="Profile()">Profile </button>
+	<button class="Tickets">Tickets </button>
+	<button class="Logout">Log Out </button>
+	
+	 <! ––Arrival un Departures pogas-->
+	<h1 id="SearchFlight" style="display: none;">Search Flight:</h1>
+	<input id="FlightSearch" class="FlightSearch" style=" display: none;" type="text" placeholder="Flight number, Destination, Airport name/code">  <! ––Lidojuma meklēšana-->
+	
+	<p class="Time" style="display: none;" id="Time">Time:    </p>                                <! ––laiks-->
+	<input type="datetime-local"id="TimeInput" style=" display: none;" class="TimeInput">
+	
+	<p class="Arrivedfrom" style="display: none;" id="Arrived_from">Arrived from: </p>              <! ––No kurienes atlidoja-->
+	<input type="text"id="Arrived_from_input" style=" display: none;" class="Arrived_from_input">
+	
+	<p class="Terminal" style="display: none;" id="Terminal">Terminal: </p>                     <! ––attiecīgais terminālis-->
+	<input type="text"id="Terminal_input" style=" display: none;" class="Terminal_input">
+	
+	<p class="Status" style="display: none;" id="Status">Status: </p>                            <! ––Status-->
+	<input type="text"id="Status_input" style=" display: none;" class="Status_input">
+	
+	 <! ––Ticket purchase pogas-->
+	<p class="Start_point" style="display: none;" id="Start_point">Starting point: </p>          <! ––Sākumpunkta izvēle-->
+	<input type="text"id="Start_point_input" style=" display: none;" class="Start_point_input">
+	
+	<p class="Destination" style="display: none;" id="Destination">Destination: </p>          <! ––Vietas izvēle-->
+	<input type="text"id="Destination_input" style=" display: none;" class="Destination_input">
+	
+	<input type="datetime-local"id="Travel_date" style=" display: none;" class="Travel_date">   <! ––Ceļojuma laika izvēle-->
+	
+	 <! ––Pirmās klases vai ekonomiskas klases izvēle-->
+	 
+	<input type="radio"id="First_class" style=" display: none;" class="First_class"  name="fav_language" >
+	 <p class="First_class_label" style=" display: none;" id="First_class_label"> First class</p>
+	 <input type="radio"id="Economy" style=" display: none;" class="Economy"  name="fav_language">
+	 <p class="Economy_label" style=" display: none;" id="Economy_label"> Economy</p>
+	
+	
+	 
+	 <p class="username" id="username" style=" display: none;"> Username:</p>
+	 <input type="text"id="username_input" style=" display: none;" class="username_input">
+	 
+	 <p class="password" id="password" style=" display: none;"> Password:</p>
+	 <input type="password"id="password_input" style=" display: none;" class="password_input">
+	 
+	 <button class="Login_button" id="Login_button" style=" display: none;"> Login</button>
 
   <table class="table" style="display: none;" id="Arrivaltable">
 
@@ -437,6 +514,12 @@ while ($row = mysqli_fetch_assoc * $result) {
       document.getElementById("Terminal_input").style.display = "block";
       document.getElementById("Status").style.display = "block";
       document.getElementById("Status_input").style.display = "block";
+	  
+	  document.getElementById("username").style.display = "none";
+	 document.getElementById("username_input").style.display = "none";
+	  document.getElementById("password").style.display = "none";
+	  document.getElementById("password_input").style.display = "none";
+	  document.getElementById("Login_button").style.display = "none";
 
       document.getElementById("Start_point").style.display = "none";
       document.getElementById("Start_point_input").style.display = "none";
@@ -455,6 +538,8 @@ while ($row = mysqli_fetch_assoc * $result) {
       box.style.backgroundColor = 'white';
       var box = document.querySelector('.Ticketpurchase');
       box.style.backgroundColor = 'white';
+	    var box = document.querySelector('.Login');
+      box.style.backgroundColor = 'white';
       const element = document.getElementById("AD");
       element.remove();
     }
@@ -471,6 +556,12 @@ while ($row = mysqli_fetch_assoc * $result) {
       document.getElementById("Terminal_input").style.display = "block";
       document.getElementById("Status").style.display = "block";
       document.getElementById("Status_input").style.display = "block";
+	  
+	  document.getElementById("username").style.display = "none";
+	 document.getElementById("username_input").style.display = "none";
+	  document.getElementById("password").style.display = "none";
+	  document.getElementById("password_input").style.display = "none";
+	  document.getElementById("Login_button").style.display = "none";
 
       document.getElementById("Start_point").style.display = "none";
       document.getElementById("Start_point_input").style.display = "none";
@@ -488,6 +579,8 @@ while ($row = mysqli_fetch_assoc * $result) {
       var box = document.querySelector('.Arrival');
       box.style.backgroundColor = 'white';
       var box = document.querySelector('.Ticketpurchase');
+      box.style.backgroundColor = 'white';
+	    var box = document.querySelector('.Login');
       box.style.backgroundColor = 'white';
       const element = document.getElementById("AD");
       element.remove();
@@ -508,6 +601,13 @@ while ($row = mysqli_fetch_assoc * $result) {
       document.getElementById("First_class_label").style.display = "block";
       document.getElementById("Economy").style.display = "block";
       document.getElementById("Economy_label").style.display = "block";
+	  
+	  document.getElementById("username").style.display = "none";
+	 document.getElementById("username_input").style.display = "none";
+	  document.getElementById("password").style.display = "none";
+	  document.getElementById("password_input").style.display = "none";
+	  document.getElementById("Login_button").style.display = "none";
+	  
       document.getElementById("SearchFlight").style.display = "none";
       document.getElementById("FlightSearch").style.display = "none";
       document.getElementById("Time").style.display = "none";
@@ -526,6 +626,8 @@ while ($row = mysqli_fetch_assoc * $result) {
       box.style.backgroundColor = 'white';
       var box = document.querySelector('.Arrival');
       box.style.backgroundColor = 'white';
+	   var box = document.querySelector('.Login');
+      box.style.backgroundColor = 'white';
       const element = document.getElementById("AD");
       element.remove();
     }
@@ -536,6 +638,46 @@ while ($row = mysqli_fetch_assoc * $result) {
       }
       setInterval('autoRefresh()', 5000);
     }
+	function Login()
+	{
+	document.getElementById("username").style.display = "block";
+	 document.getElementById("username_input").style.display = "block";
+	  document.getElementById("password").style.display = "block";
+	  document.getElementById("password_input").style.display = "block";
+	  document.getElementById("Login_button").style.display = "block";
+	
+	 document.getElementById("Start_point").style.display = "none";
+	  document.getElementById("Start_point_input").style.display = "none";
+	   document.getElementById("Destination").style.display = "none";
+	  document.getElementById("Destination_input").style.display = "none";
+	  document.getElementById("Travel_date").style.display = "none";
+	   document.getElementById("First_class").style.display = "none";
+	document.getElementById("First_class_label").style.display = "none";
+	  document.getElementById("Economy").style.display = "none";
+	document.getElementById("Economy_label").style.display = "none";
+	document.getElementById("SearchFlight").style.display = "none";
+	 document.getElementById("FlightSearch").style.display = "none";
+	  document.getElementById("Time").style.display = "none";
+	  document.getElementById("TimeInput").style.display = "none";
+	  document.getElementById("Arrived_from").style.display = "none";
+	 document.getElementById("Departuretable").style.display = "none";
+	 document.getElementById("Arrivaltable").style.display = "none";
+	 document.getElementById("Arrived_from_input").style.display = "none";
+	 document.getElementById("Terminal").style.display = "none";
+	   document.getElementById("Terminal_input").style.display = "none";
+	   document.getElementById("Status").style.display = "none";
+	   document.getElementById("Status_input").style.display = "none";
+	   var box = document.querySelector('.Login');
+      box.style.backgroundColor = 'yellow';
+	     var box = document.querySelector('.Departure');
+      box.style.backgroundColor = 'white';
+	var box = document.querySelector('.Arrival');
+      box.style.backgroundColor = 'white';
+	  var box = document.querySelector('.Ticketpurchase');
+      box.style.backgroundColor = 'white';
+	    const element = document.getElementById("AD");
+     element.remove();
+	}
   </script>
 
 </body>
